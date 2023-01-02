3b2d8ab6602acaa1868d2a1ad7fa715d76ff1738
PowerOleg <geraskin.oleg@mail.ru>
2023-01-02 16:53:57 +0500

    GUI MainView&TableModel 1.4-snapshot

--- src/main/java/com/company/GUI/MainView.java
+++ src/main/java/com/company/GUI/MainView.java
@@ -2,6 +2,8 @@
 //        1,1,0,0,GridBagConstraints.WEST, GridBagConstraints.BOTH,
 //        new Insets(1,1,1,1),0,0));
 
+
+//странно что таблица всегда отображает значения, я думал нужно будет обновлять
 package com.company.GUI;
 
 
@@ -12,19 +14,24 @@ public class MainView {
     JPanel window;
     JPanel buttonsPane1;
     JPanel buttonsPane2;
+    JPanel buttonsPane3;
     JFrame frame;
 
     JTextField textField1;
-    JButton createTable, deleteTable, refreshTable, addProductName, deleteProductName, addBrand, deleteBrand,
-    addProductType, deleteProductType, setPrice;                                        //+ цвет, страна
+    JLabel lable1;
+    JButton createTable, deleteTable, refreshTable, addProductName, deleteProductName, addProductType, deleteProductType,
+            addBrand, deleteBrand, setPrice, setQuantity;                                        //+ цвет, страна
 
     public MainView() {
         window = new JPanel();
         window.setLayout(new BorderLayout());
         buttonsPane1 = new JPanel();
         buttonsPane2 = new JPanel();
+        buttonsPane3 = new JPanel();
         buttonsPane1.setLayout(new GridLayout(3, 4));
-        textField1 = new JTextField(10);
+        textField1 = new JTextField(20);
+        lable1 = new JLabel("Нажмите кнопку действия ниже, затем введите параметр");
+
         createTable = new JButton("Create Table");
         deleteTable = new JButton("Delete Table");
         refreshTable = new JButton("Refresh Table");
@@ -35,27 +42,41 @@ public class MainView {
         addProductType = new JButton("Add Product Type");
         deleteProductType = new JButton("Delete Product Type");
         setPrice = new JButton("Set Price");
-
+        setQuantity = new JButton("Set Quantity");
 
 
 
         buttonsPane1.add(createTable);
         buttonsPane1.add(addProductName);
-        buttonsPane1.add(addBrand);
         buttonsPane1.add(addProductType);
+        buttonsPane1.add(addBrand);
         buttonsPane1.add(deleteTable);
         buttonsPane1.add(deleteProductName);
-        buttonsPane1.add(deleteBrand);
         buttonsPane1.add(deleteProductType);
+        buttonsPane1.add(deleteBrand);
         buttonsPane1.add(refreshTable);
         buttonsPane1.add(setPrice);
+        buttonsPane1.add(setQuantity);
+        buttonsPane2.add(lable1);
         buttonsPane2.add(textField1);
+
+
+
+//Table
+        TableModel tableModel = new TableModel();
+        JTable table = new JTable(tableModel);
+        JScrollPane jScrollPane = new JScrollPane(table);
+        jScrollPane.setPreferredSize(new Dimension(640, 480));
+//тут раньше был код чтобы добавить значения в таблицу
+
+        buttonsPane3.add(jScrollPane);
 
 
 
 //
         window.add("South", buttonsPane1);
-        window.add("East", buttonsPane2);
+        window.add("North", buttonsPane2);
+        window.add("Center", buttonsPane3);
         frame = new JFrame();
         frame.setContentPane(window);
         frame.setSize(960,720);
--- src/main/java/com/company/GUI/TableModel.java
+++ src/main/java/com/company/GUI/TableModel.java
@@ -1,4 +1,50 @@
 package com.company.GUI;
 
-public class TableModel {
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableModel extends AbstractTableModel {
+
+    private int columnNumber = 6;
+    List<String[]> arrayList;
+
+    public TableModel() {
+    arrayList = new ArrayList<String[]>();
+    }
+
+    @Override
+    public int getRowCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNumber;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        String[] s = arrayList.get(rowIndex);
+        return s[columnIndex];
+    }
+
+    @Override
+    public String getColumnName(int column) {
+    switch (column) {
+        case 0: return "Name";
+        case 1: return "Type";
+        case 2: return "Brand";
+        case 3: return "Quantity";
+        case 4: return "Price(1)";
+        case 5: return "Price";
+    }
+    return null;
+    }
+    public boolean addRow(String[] row) {
+    return arrayList.add(row);
+    }
+
+
+    //public void addFromSQL (ConnectionDb connection) {} поидее работы с SQL тут быть не должно
 }
