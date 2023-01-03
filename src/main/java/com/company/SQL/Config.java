af5b21c934710bd77d6db7915afdab6351ad5af3
PowerOleg <geraskin.oleg@mail.ru>
2023-01-03 13:31:49 +0500

    SQL config 1.7-snapshot

---
+++ src/main/java/com/company/SQL/Config.java
@@ -0,0 +1,9 @@
+package com.company.SQL;
+
+public class Config {
+    public static String dbName = "interviewtask1";
+    public static String dbHost = "127.0.0.1";
+    public static String dbPort = "3306";
+    public static String dbUser = "root";
+    public static String dbPass = "";
+}
---
+++ src/main/java/com/company/SQL/Const.java
@@ -0,0 +1,15 @@
+package com.company.SQL;
+
+public class Const {
+    public static final String EMPLOYEE_TABLE = "employee";
+    public static final String EMPLOYEE_ID = "id";
+    public static final String EMPLOYEE_DEPARTMENT_ID = "department_id";
+    public static final String EMPLOYEE_WORK_START_DATE = "work_start_date";
+    public static final String EMPLOYEE_NAME = "name";
+    public static final String EMPLOYEE_SALARY = "salary";
+
+    public static final String DEPARTMENT_TABLE = "department";
+    public static final String DEPARTMENT_ID = "id";
+    public static final String DEPARTMENT_NAME = "name";
+    public static final String DEPARTMENT_LEAD_ID = "lead_id";
+}
